f3a6037add31a3a33a86015f42d283bc5112bf1d
jrajeshbritto <jrajeshbritto@gmail.com>
2018-03-15 20:38:37 +0530

    componenent added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'app';
+  books = [
+    {
+      "name" : "Non-stop India",
+      "author" : "Mark Tully"
+    },
+    {
+      "name" : "The Mad Tibetan: Stories from then and now",
+      "author" : "Deepti Naval"
+    },
+    {
+      "name" : "Nirbasan",
+      "author" : "Taslima Nasreen"
+    },
+    {
+      "name" : "The Best Thing About You is You",
+      "author" : "Anupam Kher"
+    }
+  ];
+
+  public bookList;
+  constructor(private angularFire: AngularFireDatabase) {
+  }
+  ngOnInit() {
+      this.getTask().subscribe(res => {
+          this.bookList = res;
+      });
+  }
+  getTask(){
+      return this.angularFire.list('/bookList').valueChanges();
+  }
 }
